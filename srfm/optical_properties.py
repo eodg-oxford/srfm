fixed a bug in phase function normalisation in optical_properties.py/loop_mie_over_wavelengths - added weighting by radius and Q_sca, fixed orography.py/get_elevations to correctly count latitudes and also not modify the input array, fixed a bug in scaling of lonlim in readers/read_iasi_l2.py/IASI_L2, added new function to calculate altitude from pressure and temperature into utilities, updated Makefile to not create build.log when cleaning package.

srfm/optical_properties.py
@@ -921,14 +921,12 @@ def loop_mie_over_wavelengths(wavelengths_size,
         # calculate weighted sums of some variables:
         
         # Weighted sums for extinction and scattering
-        beta_ext[i] = np.sum(radius_weight * np.pi * radius**2 * Q_ext_value) * 1e-6        
-        beta_sca[i] = np.sum(radius_weight * np.pi * radius**2 * Q_sca_value) * 1e-6
+        beta_ext[i] = np.pi * np.sum(radius_weight * radius**2 * Q_ext_value) * 1e-6        
+        beta_sca[i] = np.pi * np.sum(radius_weight * radius**2 * Q_sca_value) * 1e-6
         
         # Weighted sums for phase function
         for k in range(angles):
-            phase_function[i, k] = (
-                np.sum(radius_weight * phase_function_value[:, k]) / rad_wt_sum 
-            )
+            phase_function[i, k] = np.pi * np.sum(radius_weight * radius**2 * Q_sca_value * phase_function_value[:, k]) / rad_wt_sum
        
         # If requested expand phase functions as Legendre coefficients
         if legendre_coefficients_flag:

srfm/orography.py
@@ -15,6 +15,7 @@ from cartopy import crs as ccrs, feature as cfeature
 from scipy.interpolate import RegularGridInterpolator
 from cartopy.mpl.ticker import (LatitudeFormatter, LongitudeFormatter,
                                 LatitudeLocator,LongitudeLocator)
+import matplotlib.style as mplstyle
 
 
 """
@@ -60,6 +61,7 @@ def plot_Earth():
     ax.set_xlabel('Longitude')
     ax.set_ylabel('Lattitude')
     
+    mplstyle.use("fast")
     plt.show()
     return
 
@@ -68,9 +70,13 @@ def get_elevation(coords):
     
     Args:
         coords (array-like): Lat, lon pairs at which to evaluate elevation. 
-                             2D array expected. First column ([:,0]) is latitudes, 
+                             2D array expected.
+                             For single point, 1D array or list [lat,lon] are accepted 
+                             and converted to 2D array.
+                             First column ([:,0]) is latitudes, 
                              second column ([:,1]) is longitudes.
-                             Lat must be within (-90,90). Lon must be within (-180,180)
+                             Lat must be within (-89.516,,89.866). 
+                             Lon must be within (-180,179.473).
                              
     Returns:
         ele (array-like): Elevation array. Shape coords.shape[0]. Units [m].
@@ -79,20 +85,20 @@ def get_elevation(coords):
     
     """
     
+    if isinstance(coords, list) and len(coords) == 2:
+        coords = np.array(coords)    
+    
+    if isinstance(coords, np.ndarray) and coords.ndim == 1:
+        coords = np.expand_dims(coords, axis = 0)
+    
     interp = RegularGridInterpolator((data.variables["lat"][:],
                                       data.variables["lon"][:]
                                      ),
                                      data.variables["orog"][:]
                                     )
+    coords2 = coords.copy()
+    coords2[:,1] = np.where(coords2[:,1] < 0, coords2[:,1]+360, coords2[:,1])
     
-    # if coords is list, attempt to convert to array 
-    if isinstance(coords, list):
-        coords = np.array(coords)
-    
-    if coords.ndim == 1:
-        coords = np.expand_dims(coords,axis=0)
-    
-    coords[:,1] = coords[:,1]
-    ele = interp(coords)
+    ele = interp(coords2)
     return ele
     

srfm/readers/read_iasi_l2.py
@@ -302,7 +302,7 @@ class Iasi_L2:
         self._set_cols  ( mdr, idxfov )        # extract column data
         self._set_sfc ( mdr, mdr_sfz, idxfov ) # extract surface data 
         self._set_cloud ( mdr, idxfov )        # extract cloud data
- 
+
         if not forli: continue
 
         # Continue reading Forli/Brescia parts of MDR - variable sizes
@@ -587,8 +587,8 @@ class Iasi_L2:
       [boo]  : List of 120 flags, True=fov within lon.limits
 
     """
-    imin = round ( lonlim[0]*1e6 ) # scale to match MDR units 
-    imax = round ( lonlim[1]*1e6 ) 
+    imin = round ( lonlim[0]*1e4 ) # scale to match MDR units 
+    imax = round ( lonlim[1]*1e4 ) 
     ilon = np.array ( mdr['earth_location'][:,1] )
     if imin < imax:      # conventional lon. coverage
       return ( ilon >= imin ) & ( ilon <= imax )